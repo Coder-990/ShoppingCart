2fd532d01444a80f4d5dfff1e64676de2a9a3374
Domagoj Borovcak <borovcakdomagoj@gmail.com>
2024-06-18 20:17:48 +0200

    mongodb setup

---
+++ src/test/java/hr/ht/retail/ShoppingCart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.ht.retail.ShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
